43297e7f162a395231fe1596f78536ac977f607f
hojong6636 <hojong6636@gmail.com>
2018-04-03 03:57:56 +0900

    Change Header Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,8 @@ import React, {component} from 'react';
 
 export default function HeaderComponent() {
   return (
-    <div>
-        <div className="headicon"> 
-            <i class="fab fa-audible"></i> <i class="fas fa-wifi"></i> <i class="fas fa-battery-full"></i>
-        </div>
-      <div className="head">
-        <i class="fas fa-bars fa-2x"></i>
-      </div>
+    <div className="headicon"> 
+      <span className="chief-mate chief-mate-menu"></span>
     </div>
   );
 }
