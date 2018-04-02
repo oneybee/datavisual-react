39ccd05ff9ff804ac2a6cefac82dc80fb27b2388
hojong6636 <hojong6636@gmail.com>
2018-04-03 04:05:42 +0900

    add test images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React, {component} from 'react';
 
+// import profileImg from '../../assets/images/profile.jpg';
+
+
 export default function HeaderComponent() {
   return (
     <div className="headicon"> 
       <span className="chief-mate chief-mate-menu"></span>
+      {/* <img src={profileImg} /> */}
+      <img src="assets/images/profile.jpg"/>
     </div>
   );
 }
