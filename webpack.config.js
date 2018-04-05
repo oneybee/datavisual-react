8bc62516c49fc14002df2adaf44abb7edbe95905
ruucm <ruucm.a@gmail.com>
2018-04-06 05:03:20 +0900

    change public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: `${__dirname}/src/index.js`,
   output: {
     path: `${__dirname}/build`,
-    publicPath: '/build/',
+    publicPath: '/datavisual-react/build/',
     filename: 'bundle.js',
   },
 
@@ -38,8 +38,7 @@ module.exports = {
         test: /\.(png|jpeg|jpg|gif|woff|woff2|eot|ttf|svg)$/,
         use: [
          { loader: 'url-loader', options: { 
-           limit: 8192,
-           name: 'datavisual-react/[name].[hash:7].[ext]'
+           limit: 8192
           } } 
          // limit => file.size =< 8192 bytes ? DataURI : File
         ]
